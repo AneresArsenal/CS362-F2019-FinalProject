0129029e12fad1f4ba2d2cdbec8600adc9f83ba3
Serena Tay <tays@oregonstate.edu>
2019-11-28 14:27:21 -0800

    commit unittest9

--- projects/FinalProject-Bugs/dominion/unittest9.c
+++ projects/FinalProject-Bugs/dominion/unittest9.c
@@ -7,26 +7,33 @@
 
 void unitTest9()
 {
- // initialize and set variables
+    printf("Starting Unit Test 9 - Tribute card - duplicate revealed cards \n\n");
+
+    // initialize and set variables
     struct gameState pre, post;
     int k[10] = {feast, gardens, embargo, village, minion, mine, cutpurse,
                  sea_hag, tribute, smithy};
     int tributeRevealedCards[2] = {-1, -1};
 
-    printf("Starting Unit Test 4 - playTribute function \n\n");
-
     initializeGame(2, k, 1234, &pre);
+
     printf("Game initialized \n\n");
+
+    //Set discard pile to zero.
+    pre.discardCount[pre.whoseTurn] = 0;
+    pre.discard[pre.whoseTurn][0] = -1;
 
     int handPos = 0,
         currentPlayer = 0,
         bonus = 0,
         r = 10;
 
-    int nextPlayer = currentPlayer + 1;
+    int nextPlayer = pre.whoseTurn + 1;
 
-    pre.handCount[currentPlayer] = r;
-    pre.deckCount[nextPlayer] = 11;
+    //Set first cards in hand to feast and silver
+    pre.handCount[pre.whoseTurn] = r;
+    pre.handCount[nextPlayer] = r;
+    // pre.hand[pre.whoseTurn][0] = tribute;
 
     // randomly assign cards to player and other player
     for (int i = 0; i < r; i++)
@@ -35,32 +42,23 @@ void unitTest9()
         pre.hand[nextPlayer][i] = k[9 - i];
     }
 
-    // set num actions
-    pre.numActions = 3;
+    // set test variables
+    pre.numActions = 2;
+    pre.coins = 2;
 
     memcpy(&post, &pre, sizeof(struct gameState));
     printf("Test case 1: Reveal and discard top 2 cards (1 action card and 1 treasure card) from next player's hand. \n\n");
 
     // set top 2 cards
-    post.hand[nextPlayer][10] = village;
-    post.hand[nextPlayer][9] = copper;
+    post.hand[nextPlayer][9] = village;
+    post.hand[nextPlayer][8] = copper;
 
-    
-
-    // call the refactored functions
-    playTribute(&post, currentPlayer, handPos, &tributeRevealedCards[2], nextPlayer, bonus);
+    // call the cardEffect function
+    cardEffect(tribute, 0, 0, 0, &post, 0, &bonus);
 
     // assert the results
-    // discard 2 cards from other player's deck
-    if (post.deckCount[nextPlayer] != pre.deckCount[nextPlayer] - 2)
-    {
-        printf("Bug found! Number of discarded cards from other player's deck is not 2! \n");
-        printf("Pre-call deckCount: %d \n", pre.deckCount[nextPlayer]);
-        printf("Post-call deckCount: %d \n\n", post.deckCount[nextPlayer]);
-    }
-
     // copper coin (treasure card) should increase coins by 2
-    if (post.coins != pre.coins + 2)
+    if (bonus != 2)
     {
         printf("Bug #1 found! Wrong amount of bonus added! \n");
         printf("Pre-call coin tally: %d \n", pre.coins);
@@ -79,14 +77,14 @@ void unitTest9()
     memcpy(&post, &pre, sizeof(struct gameState));
     printf("Test case 2: Reveal and discard top 2 cards from next player's hand with victory cards only. \n\n");
 
-    // call the refactored functions
-    playTribute(&post, currentPlayer, handPos, &tributeRevealedCards[2], nextPlayer, bonus);
+    // call the cardEffect function
+    cardEffect(tribute, 0, 0, 0, &post, 0, &bonus);
 
-    post.hand[nextPlayer][10] = estate;
     post.hand[nextPlayer][9] = estate;
+    post.hand[nextPlayer][8] = estate;
 
     // assert the results
-    // adds only 2 cards to current player's deck and deduct 1 tribute card played 
+    // adds only 2 cards to current player's deck and deduct 1 tribute card played
     // (net 1 additional card)
     if (post.handCount[currentPlayer] == pre.handCount[currentPlayer] + 1)
     {
@@ -96,25 +94,25 @@ void unitTest9()
     }
 
     memcpy(&post, &pre, sizeof(struct gameState));
-    printf("Test case 3: Next player does not have sufficient cards to reveal \n\n");
+    printf("Test case 3: Reveal and discard top 2 cards from next player's hand with action cards only. \n\n");
 
-    // call the refactored functions
-    playTribute(&post, currentPlayer, handPos, &tributeRevealedCards[2], nextPlayer, bonus);
+    // call the cardEffect function
+    cardEffect(tribute, 0, 0, 0, &post, 0, &bonus);
 
-    pre.deckCount[nextPlayer] = 0;
-    pre.discardCount[nextPlayer] = 0;
+    post.hand[nextPlayer][9] = minion;
+    post.hand[nextPlayer][8] = minion;
 
     // assert the results
-    // no actions taken and deduct 1 tribute card played
-    if (post.deckCount[currentPlayer] != pre.deckCount[currentPlayer] - 1)
+    // adds only 2 action phases to current player's deck and deduct 1 action phase played
+    // (net 1 action phase)
+    if (post.numActions == pre.numActions + 1)
     {
-        printf("Bug found! Only one tribute card should be discarded.\n");
-        printf("Pre-call deckCount: %d \n", pre.deckCount[currentPlayer]);
-        printf("Post-call deckCount: %d \n\n", post.deckCount[currentPlayer]);
+        printf("Valid! Number of cards in player's hand is accurate! \n");
+        printf("Pre-call handCount: %d \n", pre.numActions);
+        printf("Post-call handCount: %d \n\n", post.numActions);
     }
 
-    printf("Unit Test 4 completed! \n\n");
- 
+    printf("Unit Test 9 completed! \n\n");
 }
 
 int main()
