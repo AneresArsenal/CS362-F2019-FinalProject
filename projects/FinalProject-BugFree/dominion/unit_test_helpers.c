9534e8c16548ca1c06b36a898763150bb248f7d0
Eric C. Morgan <morganer@oregonstate.edu>
2019-11-30 15:51:40 -0800

    Updated unit test helper to show successful tests

--- projects/FinalProject-BugFree/dominion/unit_test_helpers.c
+++ projects/FinalProject-BugFree/dominion/unit_test_helpers.c
@@ -3,24 +3,20 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-void assert(int condition, const char* message, ...)
+void assert(int condition, const char *message, ...)
 {
-    if(DEBUG || !condition)
-    {
-        char messageBuf[250];
-        va_list args;
-        va_start(args, message);
-        vsnprintf(messageBuf, sizeof(messageBuf), message, args);
-        va_end(args);
+    char messageBuf[250];
+    va_list args;
+    va_start(args, message);
+    vsnprintf(messageBuf, sizeof(messageBuf), message, args);
+    va_end(args);
 
-        if(!condition)
-        {
-            printf("FAILED - %s", messageBuf);
-        }
-        else
-        {
-            printf("DEBUG - %s", messageBuf);
-        }
-        
+    if (!condition)
+    {
+        printf("FAILED - %s", messageBuf);
+    }
+    else
+    {
+        printf("PASSED - %s", messageBuf);
     }
 }
